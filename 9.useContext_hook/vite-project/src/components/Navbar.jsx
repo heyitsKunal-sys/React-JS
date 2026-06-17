55c4bcce893144cc709e48616f7be1f9edf6831b
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-06-18 00:54:45 +0530

    commit

---
+++ 9.useContext_hook/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Button'
+
+const Navbar = ({count}) => {
+    return (
+        <>
+            <div>Navbar</div>
+            <Button count ={count}/>
+
+        </>
+    )
+}
+
+export default Navbar
